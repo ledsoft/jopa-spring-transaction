e151b9fcc62e99fbe5527d59464c21ff75071601
Martin Ledvinka <martin.ledvinka@outlook.com>
2020-11-17 21:24:21 +0100

    [0.0.10] Bump version.

--- src/main/java/com/github/ledsoft/jopa/spring/transaction/EntityManagerProvider.java
+++ src/main/java/com/github/ledsoft/jopa/spring/transaction/EntityManagerProvider.java
@@ -18,7 +18,7 @@ public interface EntityManagerProvider {
     /**
      * Returns the {@link EntityManagerFactory} representing the underlying persistence unit.
      *
-     * @return
+     * @return {@code EntityManagerFactory}
      */
     EntityManagerFactory getEntityManagerFactory();
 }
