e1010fa288107a25af3b005d57cb4aebdbf38a79
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2022-12-05 15:46:10 +0100

    [Upd] Update JOPA to 0.19.2, switch to RDF4J driver in tests.

--- src/test/java/com/github/ledsoft/jopa/spring/transaction/config/PersistenceFactory.java
+++ src/test/java/com/github/ledsoft/jopa/spring/transaction/config/PersistenceFactory.java
@@ -4,7 +4,8 @@ import cz.cvut.kbss.jopa.Persistence;
 import cz.cvut.kbss.jopa.model.EntityManagerFactory;
 import cz.cvut.kbss.jopa.model.JOPAPersistenceProperties;
 import cz.cvut.kbss.jopa.model.JOPAPersistenceProvider;
-import cz.cvut.kbss.ontodriver.sesame.config.SesameOntoDriverProperties;
+import cz.cvut.kbss.ontodriver.rdf4j.Rdf4jDataSource;
+import cz.cvut.kbss.ontodriver.rdf4j.config.Rdf4jOntoDriverProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -29,10 +30,10 @@ public class PersistenceFactory {
         properties.put(JOPAPersistenceProperties.LANG, "en");
         properties.put(JOPAPersistenceProperties.SCAN_PACKAGE, "com.github.ledsoft.jopa.spring.transaction.model");
         properties.put(JOPAPersistenceProperties.CACHE_ENABLED, Boolean.FALSE.toString());
-        properties.put(SesameOntoDriverProperties.SESAME_USE_VOLATILE_STORAGE, Boolean.TRUE.toString());
+        properties.put(Rdf4jOntoDriverProperties.USE_VOLATILE_STORAGE, Boolean.TRUE.toString());
         properties.put(JOPAPersistenceProperties.JPA_PERSISTENCE_PROVIDER, JOPAPersistenceProvider.class.getName());
         properties.put(JOPAPersistenceProperties.ONTOLOGY_PHYSICAL_URI_KEY, "test-repository");
-        properties.put(JOPAPersistenceProperties.DATA_SOURCE_CLASS, "cz.cvut.kbss.ontodriver.sesame.SesameDataSource");
+        properties.put(JOPAPersistenceProperties.DATA_SOURCE_CLASS, Rdf4jDataSource.class.getCanonicalName());
         this.emf = Persistence.createEntityManagerFactory("testPU", properties);
     }
 
